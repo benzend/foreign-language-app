95ec906e965dedac5160b106d3e5f91c72fafccb
Benjamin <1234hteimaj4321@gmail.com>
2020-12-23 16:55:21 -0700

    added styling to settings

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,12 +1,21 @@
 import { Link } from "react-router-dom";
 import Nav from "./Nav";
 
+import styled from "styled-components";
+
+const LinksContainer = styled.div`
+  height: 90vh;
+  flex-direction: column;
+`;
+
 export const Settings = () => {
   return (
     <>
       <Nav />
-      <Link to="/settings/languages">Language Options</Link>
-      <Link to="/settings/signout">Sign Out</Link>
+      <LinksContainer className="flex-center">
+        <Link to="/settings/languages">Language Options</Link>
+        <Link to="/settings/signout">Sign Out</Link>
+      </LinksContainer>
     </>
   );
 };
