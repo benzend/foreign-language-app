295dc017decb54e969849146228de9806334a75a
Benjamin <1234hteimaj4321@gmail.com>
2021-06-20 09:22:53 -0600

    style: modal styling
    added styling to the modal, so that it has a dark underlay, covering the
    screen, and now it is actually centered. I also added a close button
    inside of the Modal, and put it to the top right.

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button } from "./Button";
 
 interface IModalProps {
   isOpen: boolean;
@@ -10,9 +11,15 @@ export const Modal: React.FC<IModalProps> = ({ children, isOpen, close }) => {
     <div
       className={`${
         isOpen ? "block" : "hidden"
-      } flex justify-center items-center min-h-screen min-w-screen fixed top-0 left-0`}
+      } flex justify-center items-center min-h-screen min-w-full fixed top-0 left-0 bg-black bg-opacity-50`}
     >
-      <div>{children}</div>
+      <div className="p-5 bg-white rounded relative">
+        <div className="absolute top-0 right-0">
+          <Button onClick={close}>Close</Button>
+        </div>
+
+        {children}
+      </div>
     </div>
   );
 };
